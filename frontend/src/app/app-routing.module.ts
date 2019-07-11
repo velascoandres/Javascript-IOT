48405795efd3e04b1cd3bbaa8388dae98c68e71e
Andrés Velasco <velasco.andrs@gmail.com>
2019-07-11 17:12:00 -0500

    migracion a angular 8

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RutaHabitacionComponent} from "./rutas/ruta-inicio/ruta-sitios/ruta-habitacion/ruta-habitacion.component";
+import {RutaLogHabitacionComponent} from "./rutas/ruta-inicio/ruta-sitios/ruta-habitacion/ruta-log-habitacion/ruta-log-habitacion.component";
+import {RutaSitiosComponent} from "./rutas/ruta-inicio/ruta-sitios/ruta-sitios.component";
 import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
 import {RutaRegistroComponent} from "./rutas/ruta-registro/ruta-registro.component";
 import {RutaInicioComponent} from "./rutas/ruta-inicio/ruta-inicio.component";
-import {RutaSitiosComponent} from "./rutas/ruta-inicio/ruta-sitios/ruta-sitios.component";
-import {RutaLogHabitacionComponent} from "./rutas/ruta-inicio/ruta-sitios/ruta-habitacion/ruta-log-habitacion/ruta-log-habitacion.component";
-import {RutaHabitacionComponent} from "./rutas/ruta-inicio/ruta-sitios/ruta-habitacion/ruta-habitacion.component";
+
 
 const routes: Routes = [
   {
