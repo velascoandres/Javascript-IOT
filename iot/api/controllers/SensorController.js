81bda47c023d791f29e2d504cc19f075c577a382
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-07-29 22:10:32 -0500

    Finish Project

--- iot/api/controllers/SensorController.js
+++ iot/api/controllers/SensorController.js
@@ -10,6 +10,8 @@ module.exports = {
     if(!_.has(req.body,'id') || !_.has(req.body,'codigoSensorAsociado')){
       return res.badRequest({mensaje:'Todos los campos deben ser obligatorios', error:404});
     }
+    idPin = req.body.id
+    console.log(idPin)
     console.log('Se acciono el componenteffff:');
     return res.ok(
       {
